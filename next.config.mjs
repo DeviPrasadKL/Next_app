f867fa5d44c521712f5fb74a3076c59fd87df89d
DeviPrasadKL <deviprasadoffcial@gmail.com>
2024-03-27 18:10:03 +0530

    chore: api crud

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // images: {
-    //     domains: ['myanimelist.net', 'cdn.myanimelist.net'],
-    //   },
-    
     images: {
         remotePatterns: [
             {
