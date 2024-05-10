922fda6102487c10117b75f8c84d3e17fc22d894
DeviPrasadKL <deviprasadoffcial@gmail.com>
2024-05-10 11:08:39 +0530

    chore: Image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    images: {
-        domains: ['cdn.myanimelist.net', 'myanimelist.net'],
+  // images: {
+  //     domains: ['cdn.myanimelist.net', 'myanimelist.net'],
+  //   },
+
+  // This is used for Images addition
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'cdn.myanimelist.net'
       },
+      {
+        hostname: 'myanimelist.net'
+      }
+    ],
+  },
 };
 
 export default nextConfig;
